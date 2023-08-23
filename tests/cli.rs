7534788d84a81ed4515f153073329e3d9a86eaf9
Petr Muller <afri@afri.cz>
2023-08-23 23:27:32 +0200

    test(wet add): add integration test for plain call

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,3 @@
-
 mod integration {
     use assert_cmd::Command;
     use predicates::prelude::predicate;
@@ -10,6 +9,18 @@ mod integration {
             .failure()
             .stderr(predicate::str::contains("Usage: wet"))
             .stderr(predicate::str::contains("add"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn add_plain_shows_usage_and_fails() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.arg("add")
+            .assert()
+            .failure()
+            .stderr(predicate::str::contains("Usage: wet add"))
+            .stderr(predicate::str::contains(" <THOUGHT> "));
 
         Ok(())
     }
