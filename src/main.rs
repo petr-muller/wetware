9b9353bfc45ead7c66ae3a300f924da513d9315b
Petr Muller <afri@afri.cz>
2023-08-23 22:49:37 +0200

    feat(wet): scaffold `add` and integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{command, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "wet", version)]
+pub struct Wet {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Add a new thought to the database
+    #[command(name = "add", arg_required_else_help = true)]
+    Add {
+        /// The thought to add
+        thought: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Wet::parse();
 }
---
+++ tests/cli.rs
@@ -0,0 +1,17 @@
+
+mod integration {
+    use assert_cmd::Command;
+    use predicates::prelude::predicate;
+
+    #[test]
+    fn plain_shows_usage_and_fails() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.assert()
+            .failure()
+            .stderr(predicate::str::contains("Usage: wet"))
+            .stderr(predicate::str::contains("add"));
+
+        Ok(())
+    }
+}
+
