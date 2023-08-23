97b91fa4efd9f52a0236c706d42a686a62607f82
Petr Muller <afri@afri.cz>
2023-08-24 00:52:35 +0200

    feat(wet add): implement trivial sqlite-backed storage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use clap::{command, Parser, Subcommand};
+use clap::{Args, command, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[clap(name = "wet", version)]
 pub struct Wet {
+    #[clap(flatten)]
+    globals: GlobalFlags,
+
     #[clap(subcommand)]
     command: Commands,
 }
@@ -17,6 +20,39 @@ enum Commands {
     },
 }
 
-fn main() {
-    let _args = Wet::parse();
+
+#[derive(Debug, Args)]
+struct GlobalFlags {
+    /// The path to the database
+    #[arg(long, env = "WETWARE_DB_PATH", required(false))]
+    db: Option<String>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Wet::parse();
+
+    let db = args.globals.db.unwrap_or_else(|| {
+        eprintln!("No database path provided");
+        std::process::exit(1);
+    });
+
+    match args.command {
+        Commands::Add { thought } => {
+            let conn = rusqlite::Connection::open(db).unwrap();
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS thoughts (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    thought TEXT NOT NULL
+                    )",
+                (),
+            )?;
+
+            conn.execute(
+                "INSERT INTO thoughts (thought) VALUES (?)",
+                [&thought],
+            )?;
+        }
+    }
+    Ok(())
 }
