487549f87bd662a3d0c2bd77c2cfb5700109fa08
Petr Muller <afri@afri.cz>
2023-08-24 00:12:29 +0200

    test(wet add): add integration test for simple thought add

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,7 @@
 mod integration {
     use assert_cmd::Command;
     use predicates::prelude::predicate;
+    use rusqlite::Connection;
 
     #[test]
     fn plain_shows_usage_and_fails() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,6 +22,31 @@ mod integration {
             .failure()
             .stderr(predicate::str::contains("Usage: wet add"))
             .stderr(predicate::str::contains(" <THOUGHT> "));
+
+        Ok(())
+    }
+
+    #[test]
+    fn add_stores_thought_in_database() -> Result<(), Box<dyn std::error::Error>> {
+        let db = assert_fs::NamedTempFile::new("wetware.db")?;
+        println!("{}", db.path().display());
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("This is a simple thought")
+            .assert()
+            .success();
+
+        let conn = Connection::open(db.path())?;
+        let mut stmt = conn.prepare("SELECT thought FROM thoughts")?;
+        let rows = stmt.query_map([], |row| row.get::<usize, String>(0))?;
+        let mut thoughts = Vec::new();
+        for thought in rows {
+            thoughts.push(thought);
+        }
+
+        assert_eq!(thoughts.len(), 1);
+        assert_eq!(thoughts[0].as_ref().unwrap(), "This is a simple thought");
 
         Ok(())
     }
