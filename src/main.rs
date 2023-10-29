dfaf1a7ca80cba0885ffdbf7c150bce49d4d078f
Petr Muller <afri@afri.cz>
2023-10-30 00:25:22 +0100

    feat(wet add): support linking thoughts with dates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{DateTime, Utc};
 use clap::{Args, command, Parser, Subcommand};
+use rusqlite::params;
 
 #[derive(Debug, Parser)]
 #[clap(name = "wet", version)]
@@ -17,6 +19,8 @@ enum Commands {
     Add {
         /// The thought to add
         thought: String,
+        #[arg(short, long)]
+        datetime: Option<DateTime<Utc>>
     },
 }
 
@@ -37,20 +41,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     match args.command {
-        Commands::Add { thought } => {
+        Commands::Add { thought, datetime} => {
             let conn = rusqlite::Connection::open(db).unwrap();
 
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS thoughts (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    thought TEXT NOT NULL
+                    thought TEXT NOT NULL,
+                    datetime INTEGER NOT NULL
                     )",
                 (),
             )?;
 
+            let now = datetime.unwrap_or_else(chrono::offset::Utc::now);
+
             conn.execute(
-                "INSERT INTO thoughts (thought) VALUES (?)",
-                [&thought],
+                "INSERT INTO thoughts (thought, datetime) VALUES (?1, ?2)",
+                params![&thought, &now],
             )?;
         }
     }
