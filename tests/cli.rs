72ae57bde4e63fb80b1ae90523588b843cd659b5
Petr Muller <afri@afri.cz>
2023-10-30 00:25:22 +0100

    feat(wet add): support linking thoughts with dates

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,8 @@
 mod integration {
     use assert_cmd::Command;
+    use chrono::{DateTime, Duration, Utc};
     use predicates::prelude::predicate;
-    use rusqlite::Connection;
+    use rusqlite::{Connection};
 
     #[test]
     fn plain_shows_usage_and_fails() -> Result<(), Box<dyn std::error::Error>> {
@@ -46,6 +47,78 @@ mod integration {
 
         assert_eq!(thoughts.len(), 1);
         assert_eq!(thoughts[0].as_ref().unwrap(), "This is a simple thought");
+
+        Ok(())
+    }
+
+    #[test]
+    fn add_stores_thought_in_database_with_default_date() -> Result<(), Box<dyn std::error::Error>> {
+        struct ThoughtWithDate {
+            thought: String,
+            datetime: DateTime<Utc>,
+        }
+
+        let db = assert_fs::NamedTempFile::new("wetware.db")?;
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("This is a thought with a default date")
+            .assert()
+            .success();
+        let conn = Connection::open(db.path())?;
+        let mut stmt = conn.prepare("SELECT thought, datetime FROM thoughts")?;
+        let rows = stmt.query_map([], |row| Ok(ThoughtWithDate {
+            thought: row.get(0)?,
+            datetime: row.get(1)?,
+        }))?;
+        let mut thoughts = Vec::new();
+        for thought in rows {
+            thoughts.push(thought)
+        }
+
+        assert_eq!(thoughts.len(), 1);
+        let thought = thoughts[0].as_ref().unwrap();
+        assert_eq!(thought.thought, "This is a thought with a default date");
+        let now = chrono::Utc::now();
+        let age = now - thought.datetime;
+        assert!(!age.is_zero());
+        assert!(age < Duration::seconds(1));
+
+        Ok(())
+    }
+
+    #[test]
+    fn add_stores_thought_in_database_with_given_date() -> Result<(), Box<dyn std::error::Error>> {
+        struct ThoughtWithDate {
+            thought: String,
+            datetime: DateTime<Utc>,
+        }
+
+        let db = assert_fs::NamedTempFile::new("wetware.db")?;
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("--datetime")
+            .arg("2023-10-30T00:02:42+01:00")
+            .arg("This is a thought with a given date")
+            .assert()
+            .success();
+        let conn = Connection::open(db.path())?;
+        let mut stmt = conn.prepare("SELECT thought, datetime FROM thoughts")?;
+        let rows = stmt.query_map([], |row| Ok(ThoughtWithDate {
+            thought: row.get(0)?,
+            datetime: row.get(1)?,
+        }))?;
+        let mut thoughts = Vec::new();
+        for thought in rows {
+            thoughts.push(thought)
+        }
+
+        assert_eq!(thoughts.len(), 1);
+        let thought = thoughts[0].as_ref().unwrap();
+        assert_eq!(thought.thought, "This is a thought with a given date");
+        let expected = chrono::DateTime::parse_from_rfc3339("2023-10-29T23:02:42+00:00").unwrap();
+        assert_eq!(thought.datetime, expected);
 
         Ok(())
     }
