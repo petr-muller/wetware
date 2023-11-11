0430b43c48750aa27b7aeab53691470ca2998a3b
Petr Muller <afri@afri.cz>
2023-11-11 01:40:47 +0100

    feat(wet add): support basic entity links (#15)

--- tests/cli.rs
+++ tests/cli.rs
@@ -122,5 +122,68 @@ mod integration {
 
         Ok(())
     }
+
+    #[test]
+    fn add_stores_thought_with_entity_in_database() -> Result<(), Box<dyn std::error::Error>> {
+        struct StringWithId {
+            id: isize,
+            content: String,
+        }
+        let db = assert_fs::NamedTempFile::new("wetware.db")?;
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("This is a thought about [subject]")
+            .assert()
+            .success();
+        let conn = Connection::open(db.path())?;
+        let mut stmt = conn.prepare("SELECT id, thought FROM thoughts")?;
+        let rows = stmt.query_map([], |row| Ok(StringWithId {
+            id: row.get(0)?,
+            content: row.get(1)?,
+        }))?;
+        let mut thoughts = Vec::new();
+        for thought in rows {
+            thoughts.push(thought)
+        }
+        assert_eq!(thoughts.len(), 1);
+        let thought = thoughts[0].as_ref().unwrap();
+        assert_eq!(thought.content, "This is a thought about [subject]");
+
+        let mut stmt = conn.prepare("SELECT id, name FROM entities")?;
+        let rows = stmt.query_map([], |row| Ok(StringWithId {
+            id: row.get(0)?,
+            content: row.get(1)?,
+        }))?;
+        let mut entities = Vec::new();
+        for entity in rows {
+            entities.push(entity)
+        }
+        assert_eq!(entities.len(), 1);
+        let entity = entities[0].as_ref().unwrap();
+        assert_eq!(entity.content, "subject");
+
+        struct ManyToManyItem {
+            left: isize,
+            right: isize,
+        }
+
+        let mut stmt = conn.prepare("SELECT thought_id, entity_id FROM thoughts_entities")?;
+        let rows = stmt.query_map([], |row| Ok(ManyToManyItem {
+            left: row.get(0)?,
+            right: row.get(1)?,
+        }))?;
+        let mut links = Vec::new();
+        for link in rows {
+            links.push(link)
+        }
+        assert_eq!(links.len(), 1);
+        let link = links[0].as_ref().unwrap();
+        assert_eq!(thought.id, link.left);
+        assert_eq!(entity.id, link.right);
+
+        Ok(())
+    }
+    //TODO(muller): Adding two thoughts with single entity creates just one entity row
 }
 
