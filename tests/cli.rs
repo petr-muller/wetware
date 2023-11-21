dd92c5d5b2c0cf016a4cfb666b4665e093241d8c
Petr Muller <muller@afri.cz>
2023-11-21 01:54:54 +0100

    feat(wet thougts): --on=entity (#23)

--- tests/cli.rs
+++ tests/cli.rs
@@ -262,5 +262,37 @@ mod integration {
 
         Ok(())
     }
+    #[test]
+    fn thoughts_on_shows_thoughts_on_entity() -> Result<(), Box<dyn std::error::Error>> {
+        let db = assert_fs::NamedTempFile::new("wetware.db")?;
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("This is a thought about [subject]")
+            .assert()
+            .success();
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("This is another thought about [another subject]")
+            .assert()
+            .success();
+        let mut cmd = Command::cargo_bin("wet")?;
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("add")
+            .arg("This is another thought about [subject]")
+            .assert()
+            .success();
+        let mut cmd = Command::cargo_bin("wet")?;
+        let expected_output = "This is a thought about [subject]\nThis is another thought about [subject]\n";
+        cmd.env("WETWARE_DB_PATH", db.path())
+            .arg("thoughts")
+            .arg("--on=subject")
+            .assert()
+            .success()
+            .stdout(predicate::eq(expected_output));
+
+        Ok(())
+    }
 }
 
